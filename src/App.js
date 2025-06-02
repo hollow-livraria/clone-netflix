f430970f9737003ba1fbc6f89e890c0518309e87
cironicizima <c.nicizima@aluno.ifsp.edu.br>
2025-06-02 15:25:48 -0300

    inserção header e featured infos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow";
 import FeaturedMovie from "./components/FeaturedMovie";
 import "./App.css";
+import Header from "./components/Header";
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
@@ -27,6 +28,7 @@ export default () => {
 
   return (
     <div className="page">
+      <Header />
       {featuredData && <FeaturedMovie item={featuredData} />}
       <section className="lists">
         {movieList.map((item, key) => (
--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,16 +1,43 @@
 import React from "react";
-import './FeaturedMovie.css';
+import "./FeaturedMovie.css";
 
-export default ({item}) => {
-    return (
-        <section className="featured"
-            style={{
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`,
-            }}
-        >
-            <div className="featured-vertical"></div>
-        </section>
-    )
-}
+export default ({ item }) => {
+    console.log(item);
+
+    let firstDate = new Date(item.first_air_date);
+    let genres = item.genres.map((genre) => genre.name);
+  return (
+    <section
+      className="featured"
+      style={{
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`,
+      }}
+    >
+      <div className="featured-vertical">
+        <div className="featured-horizontal">
+          <div className="featured-name">{item.original_name}</div>
+          <div className="featured-info">
+            <div className="featured-points">{item.vote_average} pontos</div>
+            <div className="featured-year"> {firstDate.getFullYear()}</div>
+            <div className="featured-seasons">
+              {item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? "s" : ""}
+            </div>
+            <div className="featured-description">{item.overview}</div>
+            <div className="featured-buttons">
+                <a href={`/watch/${item.id}`} className="featured-watchbtn">
+                {' '}
+                ► Assistir</a>
+                <a href={`/list/add/${item.id}`} className="featured-listbtn">
+                + Minha Lista</a>
+            </div>
+            <div className="featured-genres">
+                <strong>Gêneros:</strong> {genres.join(', ')}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
