1aba13b56ba21f3b3b2f4a7438828105b09715bc
livraria <nicollasfsmoreira@gmail.com>
2025-06-09 16:28:42 -0300

    scroll completo, descriçao arrumada

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Header from "./components/Header";
 export default () => {
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -19,22 +20,44 @@ export default () => {
         Math.random() * originals[0].items.results.length
       );
       let chosen = originals[0].items.results[randomChosen];
-      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
-      console.log(chosen)
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
+      console.log(chosen);
       setFeaturedData(chosenInfo);
     };
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 200) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    };
+
+    window.addEventListener("scroll", scrollListener);
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  });
+
   return (
     <div className="page">
-      <Header />
+      <Header black={blackHeader} />
       {featuredData && <FeaturedMovie item={featuredData} />}
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
+      <footer>
+        Feito por Livraria
+        <br />
+        Direitos de imagem Netflix
+        <br />
+        Dados retirados do site Themoviedb.org
+      </footer>
     </div>
   );
 };
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "../components/MovieRow.css";
 
 export default ({ title, items }) => {
+  const [scrollX, setScrollX] = useState(0);
+  const handleLeftArrow = () => {
+    let x = scrollX + Math.round(window.innerWidth / 2);
+    if (x > 0) {
+      x = 0;
+    }
+    setScrollX(x);
+  };
+  const handleRightArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    let listW = items.results.length * 250;
+    if (window.innerWidth - listW > x) {
+      x = window.innerWidth - listW - 60;
+    }
+    setScrollX(x);
+  };
+
   return (
     <div className="movieRow">
       <h2>{title}</h2>
+      <div className="movieRow-left" onClick={handleLeftArrow}>
+        <img src="setaHK.png" />
+      </div>
+      <div className="movieRow-right" onClick={handleRightArrow}>
+        <img src="setaHK.png" />
+      </div>
       <div className="movieRow-listarea">
-        <div className="movieRow-list">
+        <div
+          className="movieRow-list"
+          style={{ marginLeft: scrollX, width: items.results.length * 250 }}>
           {items.results.length > 0 &&
             items.results.map((item, key) => (
               <div key={key} className="movieRow-item">
